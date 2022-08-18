0500b04cdf7b536e5a774fb59b028a5c92bf437a
Iliya Sergeev <50948446+lapatun1980@users.noreply.github.com>
2022-08-18 13:54:55 +0000

    2 program

--- main.py
+++ main.py
@@ -1,14 +1,4 @@
 a= input ("Enter a ")
-b=input ("Enter b ")
-print ("a= "+a)
-print("b= "+b)
-input('tap the Enter to switch nambers')
-c=a
-a=b
-b=c
-print ("a= "+a)
-print("b= "+b)
-#1345566
-#dcdjkvhdjkvhk
-#145
-#678
+a1=str(a)[0]
+a2=str(a)[1]
+print (int(a1)+int(a2))
