55794e0034b357f78abb6d724e94736626659e06
Iliya Sergeev <50948446+lapatun1980@users.noreply.github.com>
2022-08-22 14:37:39 +0000

    leap year

--- main.py
+++ main.py
@@ -1,14 +1,8 @@
-height=float(input("Enter you height m "))
-weight=float(input("Enter you weight kg "))
-bmi=round(weight/height**2)
-if bmi <18.5: 
-  print (f" {bmi} UNDERWEIGHT")
-if bmi>=18.5 and bmi <25:
-  print (f" {bmi} normal weight")
-if bmi>=25 and bmi<30:
-  print (f" {bmi} over weight")
-if bmi>=30 and bmi<35:
-  print (f" {bmi} obese")
-
-if bmi>=35:
-  print (f" {bmi} clinicall obese")
+year=int(input("Enter the year "))
+if year%4!=0:
+  print ("is NOT leap year ")
+elif year%100!=0:
+  print("is  leap year ")
+elif year%400==0:
+    print("is  leap year ")
+else: print("is  NOT leap year ")
