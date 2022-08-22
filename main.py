d394ca27b6251f3efc1237fe22f76a6333703cd8
Iliya Sergeev <50948446+lapatun1980@users.noreply.github.com>
2022-08-22 12:22:05 +0000

    90-age

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-a= input ("Enter a ")
-a1=str(a)[0]
-a2=str(a)[1]
-print (int(a1)+int(a2))
+age=int(input("enter you age "))
+left_age=90-age
+day=left_age*365
+weeks=left_age*52
+month=left_age*12
+print (f"you left {day} days, {weeks} weeks and {month} month")
