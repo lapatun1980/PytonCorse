b598252e9b47b07de31880618d56db70137cee4a
Iliya Sergeev <50948446+lapatun1980@users.noreply.github.com>
2022-08-23 11:28:41 +0000

    Name and destiny

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-year=int(input("Enter the year "))
-if year%4!=0:
-  print ("is NOT leap year ")
-elif year%100!=0:
-  print("is  leap year ")
-elif year%400==0:
-    print("is  leap year ")
-else: print("is  NOT leap year ")
+print("Hello check you love & destiny")
+name1=input("Enter the fierst name ").lower()
+name2=input("Enter the second name ").lower()
+t=name1.count("t")+name2.count("t")
+r=name1.count("r")+name2.count("r")
+u=name1.count("u")+name2.count("u")
+e=name1.count("e")+name2.count("e")
+l=name1.count("l")+name2.count("l")
+o=name1.count("o")+name2.count("o")
+v=name1.count("v")+name2.count("v")
+true=t+r+u+e
+love=l+o+v+e
+destiny=f"{true}{love}"
+print("Destiny ", destiny)
