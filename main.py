e70dc02ea8ec0c47b7ee879452123b5355499d19
Iliya Sergeev <50948446+lapatun1980@users.noreply.github.com>
2022-08-24 14:01:09 +0000

    payer3

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ random.seed(test_seed)
 
 names=input("Enters all name whith ', '")
 name=names.split(", ")
-paier=random.randint(0,len(name))
+payer=random.randint(0,len(name))
 print("The payer is  ", name[payer])
