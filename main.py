99a9cf9a1d7fc5cc5ebeec990e53e78c1ee6064d
Iliya Sergeev <50948446+lapatun1980@users.noreply.github.com>
2022-08-24 13:51:24 +0000

    The paier

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ import random
 a=random.randint(1,100)
 test_seed=int(input("Enter the Seed number "))
 random.seed(test_seed)
-rais=random.randint(0,1)
-if rais==0:
-  print ("Heds")
-else: 
-  print ("Tails")
+paier=random.randint(0,5)
+names=input("Enters all name whith ', '")
+name=names.split(", ")
+print("The paier is  ", name[paier])
