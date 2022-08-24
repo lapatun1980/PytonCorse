fb821ef6c9b16209c8937e181dc324deb0266e35
Iliya Sergeev <50948446+lapatun1980@users.noreply.github.com>
2022-08-24 13:33:35 +0000

    random

--- main.py
+++ main.py
@@ -1,39 +1,9 @@
-print('''
-*******************************************************************************
-          |                   |                  |                     |
- _________|________________.=""_;=.______________|_____________________|_______
-|                   |  ,-"_,=""     `"=.|                  |
-|___________________|__"=._o`"-._        `"=.______________|___________________
-          |                `"=._o`"=._      _`"=._                     |
- _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
-|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
-|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
-          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
- _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
-|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
-|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
-____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
-/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
-____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
-/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
-____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
-/______/______/______/______/______/______/______/______/______/______/_____ /
-*******************************************************************************
-''')
-print("Welcome to Treasure Island.")
-print("Your mission is to find the treasure.") 
-
-#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
-step=input("Where do you go Right or Left ").lower()
-if step=="right":
-  step=input("You see lake? what do you do wait the boat or swim? Wait/Swim ")
-  if step=="wait":
-    step=input("You see 3 doors, which do you chise? 'red', 'green' or 'back' ")
-    if step=="green":
-      print("CONGRATULASHION YUO WIN!!!")
-    else: 
-     print("Wronge doo, GAME OVER")
-  else: 
-   print("You attaced by shark, GAME OVER")
-else:
-  print("Wrong way, GAME OVER")
+import random
+a=random.randint(1,100)
+test_seed=int(input("Enter the Seed number "))
+random.seed(test_seed)
+rais=random.randint(0,1)
+if rais==0:
+  print ("Heds")
+else: 
+  print ("Tails")
