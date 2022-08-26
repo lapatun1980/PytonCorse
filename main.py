8b3e4b3e8b0bf6e9e00a98f29b287f6d285d377a
Iliya Sergeev <50948446+lapatun1980@users.noreply.github.com>
2022-08-26 07:39:13 +0000

    rock papper scissors

--- main.py
+++ main.py
@@ -1,11 +1,44 @@
-a1=['⬜️', '⬜️', '⬜️']
-a2=['⬜️', '⬜️', '⬜️']
-a3=['⬜️', '⬜️', '⬜️']
-b=[a1,a2,a3]
-print(f"{a1}\n{a2}\n{a3}\n")
-place=input("Enter column & rou")
-x=int(place[0])
-y=int(place[1])
-change=b[x-1]
-change[y-1]="X"
-print(f"{a1}\n{a2}\n{a3}\n")
+import random
+rock0 = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper1 = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors2 = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+you=int(input("Enter rock - 0  or paper - 1 or scissors - 2  "))
+if you==0: print(">>>Your choice - ROCK",rock0)
+elif you==1: print(">>>Your choice - PAPER",paper1)
+elif you==2: print (">>>Your choice - SCISSOR",scissors2)
+  # PC choice
+pc_choice=random.randint(0,2)
+if pc_choice==0: print(">>>pc_choice choice - ROCK",rock0)
+elif pc_choice==1: print(">>>pc_choice choice - PAPER",paper1)
+elif pc_choice==2: print (">>>pc_choice choice - SCISSOR",scissors2)
+  # Who are win
+if you==pc_choice: print ("Drow")
+elif you==0 and pc_choice==1: print ("You losse")
+elif you==0 and pc_choice==2: print ("You WIN!!!")
+elif you==1 and pc_choice==0: print ("You WIN!!!")
+elif you==1 and pc_choice==2: print ("You losse")
+elif you==2 and pc_choice==0: print ("You WIN!!!")
+elif you==2 and pc_choice==1: print ("You losse")
