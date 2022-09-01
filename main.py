16a36f7559968a68923d9985105d62c7fe947717
Iliya Sergeev <50948446+lapatun1980@users.noreply.github.com>
2022-09-01 14:07:06 +0000

    encode caesare

--- main.py
+++ main.py
@@ -1,91 +1,44 @@
-import random
-stages = ['''
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========
-''']
-words=['ball', 'apple', 'botle', 'woman', 'brad' ]
-word=random.choice(words)
-finde_word=[]
-for i in range(0,len(word)):
-    finde_word+="_"
-# print(finde_word)
-    people=0
-    last_latter=len(word)
-while (people<7 and last_latter!=0):
-    counter=0
-    latter=input("Enter letter")
-    for i in range(0, len(word)):
-        if latter==word[i]:
-            finde_word[i]=word[i]
-            counter+=1
-            last_latter-=1
-    if counter!=0:
-        print (finde_word)
-    else:
-        print("wronge")
-        people+=1
-        print(stages[7-people])
-win_word=""
-for i in range(0,len(word)):
-    win_word+=finde_word[i]
-
-if people==7:
-    print("YOU LOSSE")
-else:
-    print ("YUO WIN!", win_word)
-  
-
-     
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+yes="yes"
+#DECRIP---------------------------
+def encrypt(etext):
+   for j in range(len_text_list):
+     for i in range(26):
+        if text_list[j]==alphabet[i]:
+            if (i+shift)<26:
+                text_list[j]=alphabet[i+shift]
+                break
+            else:                          
+                text_list[j]=alphabet[i+shift-26]
+                break
+#ENCRIPT--------------------------------
+def decryption(text):
+   for j in range(len_text_list):
+     for i in range(26):
+        if text_list[j]==alphabet[i]:
+            if (i+shift)<26:
+                text_list[j]=alphabet[i-shift]
+                break
+            else:                          
+                text_list[j]=alphabet[i-shift-26]
+                break
+#end defs------
     
+while (yes=="yes"):
+    direction=input("Type 'encode' for encoding or type 'dencode' for dencoding ")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    
+    text_list=list(text)
+    len_text_list=len(text_list) 
+    
+    if direction=="encode":
+        encrypt(text)
+    else:
+        decryption(text)
+    # Printing result
+    result=""
+    for i in text_list:
+        result+=i
+    print(result)   
+    yes=input("If you whant do repiat tape 'yes', if no 'no'").lower()
